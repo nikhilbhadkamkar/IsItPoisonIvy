116876574e93b41432c13e0f1175c27836602573
Abhijeet Manohar <abhijeetheromanohar@gmail.com>
2021-12-13 10:34:02 -0600

    Plant Fragment - Made changes to display information

--- app/src/main/java/com/example/isitpoisonivy/PlantFragment.java
+++ app/src/main/java/com/example/isitpoisonivy/PlantFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +17,10 @@ public class PlantFragment extends Fragment {
 
     private int plantPosition;
     Plant mplant;
+    private TextView plantIdText;
+    private TextView plantNameText;
+    private TextView botanicalNameText;
+    private TextView symptomsText;
 
     public PlantFragment(Integer position) {
         this.plantPosition = position;
@@ -41,6 +46,15 @@ public class PlantFragment extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_plant, container, false);
         mplant = MainActivity.getPlants().get(plantPosition);
+        plantIdText = view.findViewById(R.id.plantId);
+        plantNameText = view.findViewById(R.id.plantName);
+        botanicalNameText = view.findViewById(R.id.botanicalName);
+        symptomsText = view.findViewById(R.id.symptoms);
+
+        plantIdText.setText("Plant Id: "+mplant.getPlant_id());
+        plantNameText.setText("Plant Id: "+mplant.getPlant_name());
+        botanicalNameText.setText("Plant Id: "+mplant.getBotanical_name());
+        symptomsText.setText("Plant Id: "+mplant.getSymptoms());
 
         return view;
     }
